90cb1a70be97f444347b83d90e56de6bb7bc9beb
이주현 <leon6095@gmail.com>
2020-12-29 13:46:38 +0000

    10172 solved

---
+++ 10000-/10172.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	std::ios::sync_with_stdio(false);
+	
+	printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+	
+	return 0;
+}
