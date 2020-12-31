d66f65e773082c26141fd14d48afafa2743a99f0
이주현 <leon6095@gmail.com>
2020-12-31 11:22:32 +0000

    15552 solved

---
+++ 10000-/15552.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int T;
+	scanf("%d", &T);
+	while(T--){
+		int A, B;
+		scanf("%d%d", &A, &B);
+		printf("%d\n", A+B);
+	}
+	
+	return 0;
+}
