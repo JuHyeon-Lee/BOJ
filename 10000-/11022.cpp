acbf4dcd3ece7bdce8c792556ec6d5fb86004003
이주현 <leon6095@gmail.com>
2020-12-31 11:52:48 +0000

    11022 solved

---
+++ 10000-/11022.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int T;
+	scanf("%d", &T);
+	
+	for(int i=1; i<=T; i++){
+		int A, B;
+		scanf("%d%d", &A, &B);
+		printf("Case #%d: %d + %d = %d\n", i, A, B, A+B);
+	}
+	
+	return 0;
+}
