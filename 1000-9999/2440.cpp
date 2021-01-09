2e68f1235a99f3ee10584148ab775cbc17d218e9
이주현 <leon6095@gmail.com>
2021-01-09 15:10:31 +0000

    2440 solved

---
+++ 1000-9999/2440.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int N;
+	scanf("%d", &N);
+	
+	for(int i=N; i>0; i--){
+		for(int j=0; j<i; j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
