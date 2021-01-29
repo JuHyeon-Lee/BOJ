2c1c49dffdcca6a6e22fd3462913a880edd06fde
이주현 <leon6095@gmail.com>
2021-01-29 05:52:34 +0000

    10989 solved

---
+++ 10000-/10989.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int cnt[10001];
+
+int main(){
+	
+	int N;
+	scanf("%d", &N);
+	
+	for(int i=0; i<N; i++){
+		int num;
+		scanf("%d", &num);
+		cnt[num]++;
+	}
+	
+	for(int i=1; i<=10000; i++){
+		for(int j=0; j<cnt[i]; j++){
+			printf("%d\n", i);
+		}
+	}
+	
+	return 0;
+}
