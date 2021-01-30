abc036a10d7070d9d19fcf0a35bec10b9fe462a1
이주현 <leon6095@gmail.com>
2021-01-30 06:34:42 +0000

    11651 solved

---
+++ 10000-/11651.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool comp(pair<int,int> a, pair<int,int> b){
+	if(a.second==b.second){
+		return a.first<b.first;
+	} else {
+		return a.second<b.second;
+	}
+}
+
+int main(){
+	
+	int N;
+	scanf("%d", &N);
+	
+	vector<pair<int,int>> v;
+	
+	for(int i=0; i<N; i++){
+		int x, y;
+		scanf("%d%d", &x, &y);
+		v.push_back({x, y});
+	}
+	
+	sort(v.begin(), v.end(), comp);
+	
+	for(int i=0; i<N; i++){
+		printf("%d %d\n", v[i].first, v[i].second);
+	}
+	
+	return 0;
+}
