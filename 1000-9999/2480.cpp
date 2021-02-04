9a1cc9996f9c203befe148b0a3d61a009be95d13
이주현 <leon6095@gmail.com>
2021-02-04 12:21:03 +0000

    2480 solved

---
+++ 1000-9999/2480.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a, b, c;
+	scanf("%d%d%d", &a, &b, &c);
+	
+	if(a==b && b==c && c==a) printf("%d\n", 10000 + a * 1000);
+	else if(a==b) printf("%d\n", 1000 + a * 100);
+	else if(b==c) printf("%d\n", 1000 + b * 100);
+	else if(c==a) printf("%d\n", 1000 + c * 100);
+	else printf("%d\n", max(a, max(b, c)) * 100);
+	
+	return 0;
+}
