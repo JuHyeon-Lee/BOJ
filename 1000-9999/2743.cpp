8978ff7fafb2b77c458d61d8138d57ce6d6fbd40
이주현 <leon6095@gmail.com>
2021-02-09 14:36:54 +0000

    2743 solved

---
+++ 1000-9999/2743.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	char str[101];
+	
+	scanf("%s", str);
+	
+	for(int i=0; i<101; i++){
+		if(str[i]=='\0'){
+			printf("%d", i);
+			break;
+		}
+	}
+	
+	return 0;
+}
