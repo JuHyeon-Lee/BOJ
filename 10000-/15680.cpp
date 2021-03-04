c7862933c0345003e76fe36f17bf7676beec69dc
이주현 <leon6095@gmail.com>
2021-03-04 14:44:44 +0000

    15680 solved

---
+++ 10000-/15680.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int N;
+	scanf("%d", &N);
+	
+	if(!N) printf("YONSEI");
+	else printf("Leading the Way to the Future\n");
+	
+	return 0;
+}
