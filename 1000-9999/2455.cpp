610b20e6a2e97b42b42294f5867b2d07766dc7c3
이주현 <leon6095@gmail.com>
2021-07-25 05:27:18 +0000

    2455 solved

---
+++ 1000-9999/2455.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0);
+	
+	int cnt = 0;
+	int max = 0;
+	for(int i=0; i<4; i++){
+		int out, in;
+		cin >> out >> in;
+		
+		cnt -= out;
+		cnt += in;
+		
+		if(cnt>max) max = cnt;
+	}
+	
+	cout << max << "\n";
+	
+	return 0;
+}
